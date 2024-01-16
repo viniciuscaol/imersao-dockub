35e7d9f3157520c909f4ec776b354a6dcbbead8d
Vinicius <viniciuscaol@gmail.com>
2024-01-16 15:45:09 -0300

    Criação do dokerfile e compose

--- kube-news/src/server.js
+++ kube-news/src/server.js
@@ -78,6 +78,6 @@ app.get('/', async (req, res) => {
 });
 
 models.initDatabase();
-app.listen(8081);
+app.listen(8080);
 
-console.log('Aplicação rodando na porta 8081');
+console.log('Aplicação rodando na porta 8080');
